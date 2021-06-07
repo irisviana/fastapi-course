0ecb48640744c10d97b9e3709bc3d25d0cb32f1b
irisviana <vianasantana21@gmail.com>
2021-06-07 12:33:28 -0300

    connecting to database

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine
+from db.base_class import Base
 
-app = FastAPI(title=settings.PROJECT_TITTLE, varion=settings.PROJECT_VERSION)
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def star_application():
+    app = FastAPI(title=settings.PROJECT_TITTLE,
+                  varion=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+app = star_application()
 
 
 @app.get("/")
 def hello_api():
     return {"detail": "Hello word"}
+
